8e70b1a928065b7f958098c99934990f2ee835c7
UnlimitedAvailableUsername <markraileygeneroso@gmail.com>
2023-03-26 16:59:50 +0800

    shitty 127.0.0.1:8000

--- frontend/src/components/elements/TutorOnCard.js
+++ frontend/src/components/elements/TutorOnCard.js
@@ -1,34 +1,32 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-
-function Tutor({user}) {
+function Tutor({ user }) {
   return (
     <Card className="my-3 p-3 rounded">
-    <Link to={`tutor/${user.id}`}>
-      <Card.Img style={{height:256}} src={user.profile_picture} />
-    </Link>
-    <Card.Body>
       <Link to={`tutor/${user.id}`}>
-
-        
-        <Card.Title>
-          <strong>{user.first_name} {user.last_name}</strong>
-        </Card.Title>
+        <Card.Img style={{ height: 256 }} src={`http://127.0.0.1:8000${user.profile_picture}`} />
       </Link>
-      <Card.Text as="div">
-        <div> 
-
-        </div>
-        <div> Main Subject: {user.subject} <br></br> </div>
-      </Card.Text>
-      <Card.Text >
-     {user.bio}
-              </Card.Text>
-    </Card.Body>
-  </Card>
-  )
+      <Card.Body>
+        <Link to={`tutor/${user.id}`}>
+          <Card.Title>
+            <strong>
+              {user.first_name} {user.last_name}
+            </strong>
+          </Card.Title>
+        </Link>
+        <Card.Text as="div">
+          <div></div>
+          <div>
+            {" "}
+            Main Subject: {user.subject} <br></br>{" "}
+          </div>
+        </Card.Text>
+        <Card.Text>{user.bio}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
 }
 
 export default Tutor;
--- frontend/src/components/screens/TutorListScreen/TutorListScreen.js
+++ frontend/src/components/screens/TutorListScreen/TutorListScreen.js
@@ -1,65 +1,69 @@
 import React, { useEffect, useState } from "react";
 import Form from "react-bootstrap/Form";
-import {useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector } from "react-redux";
 import { listTutors } from "../../../features/redux/actions/tutorActions";
 import { Row, Col, Container } from "react-bootstrap";
 import Tutor from "../../elements/TutorOnCard";
-import '../../../assets/components/screens/TutorListScreen/tutorlist.css';
-
+import "../../../assets/components/screens/TutorListScreen/tutorlist.css";
 
 function TutorListScreen() {
-  const [search, setSearch] = useState ('')
+  const [search, setSearch] = useState("");
 
-  const dispatch = useDispatch() 
+  const dispatch = useDispatch();
 
-  const tutorList = useSelector(state => state.tutorList)
-  const {users} = tutorList
+  const tutorList = useSelector((state) => state.tutorList);
+  const { users } = tutorList;
 
   useEffect(() => {
-    dispatch(listTutors())
-   
-
+    dispatch(listTutors());
   }, [dispatch]);
   return (
     <div>
-      <div className='tutor-bg'></div>
-      <div className='tutor-bg-text-overlay text-center' >
-          <h1 className='text-uppercase tutor-text-h1'>our tutors</h1>
-          <h4 >Click on each tutor's profile to learn more about the tutor's education background and experience.</h4>
+      <div className="tutor-bg"></div>
+      <div className="tutor-bg-text-overlay text-center">
+        <h1 className="text-uppercase tutor-text-h1">our tutors</h1>
+        <h4>
+          Click on each tutor's profile to learn more about the tutor's
+          education background and experience.
+        </h4>
       </div>
       <Container>
-
-      <Form><Row>
-        <Col>
-        <Container>
-            <Form.Control className="shadow" style={{   width:1000, marginLeft:140}}onChange={(e) => setSearch(e.target.value)}
-              type="search"
-              placeholder="Search for Tutors"
-              aria-label="Search"
-            /> 
-            </Container>
-            </Col> 
-           
-            </Row>
-          </Form>
-
+        <Form>
           <Row>
-         {users.filter((user)=>
-        {
-          return search.toLowerCase() === '' ? user : user.first_name.toLowerCase().includes(search) ||
-          user.last_name.toLowerCase().includes(search) ||
-          user.bio.toLowerCase().includes(search) ||
-          user.subject.toLowerCase().includes(search);
-        }).map((user) => (
-            <Col key={user.id} sm={12} md={6} lg={4} xl={3}>
-              <Tutor user={user} />
+            <Col>
+              <Container>
+                <Form.Control
+                  className="shadow"
+                  style={{ width: 1000, marginLeft: 140 }}
+                  onChange={(e) => setSearch(e.target.value)}
+                  type="search"
+                  placeholder="Search for Tutors"
+                  aria-label="Search"
+                />
+              </Container>
             </Col>
-          ))}
-        </Row>
+          </Row>
+        </Form>
 
+        <Row>
+          {users
+            .filter((user) => {
+              return search.toLowerCase() === ""
+                ? user
+                : user.first_name.toLowerCase().includes(search) ||
+                    user.last_name.toLowerCase().includes(search) ||
+                    user.bio.toLowerCase().includes(search) ||
+                    user.subject.toLowerCase().includes(search);
+            })
+            .map((user) => (
+              <Col key={user.id} sm={12} md={6} lg={4} xl={3}>
+                <Tutor user={user} />
+              </Col>
+            ))}
+        </Row>
       </Container>
     </div>
-  )
+  );
 }
 
 export default TutorListScreen;

d313943131be04938927dc1524b0884c27889234
UnlimitedAvailableUsername <markraileygeneroso@gmail.com>
2023-03-29 18:56:54 +0800

    zooooooom

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -172,7 +172,9 @@ class CartSchedule(models.Model):
     isPaid = models.BooleanField(default=False)
     paidAt = models.DateTimeField(auto_now_add=False,null=True, blank=True)
     createdAt = models.DateTimeField(auto_now_add=True,null=True, blank=True)
+    meeting_link = models.TextField(("Zoom Link"), blank=True, null=True)
     _id =  models.AutoField(primary_key=True,editable=False)
+
 
     def __str__(self):
         return str(self.createdAt)

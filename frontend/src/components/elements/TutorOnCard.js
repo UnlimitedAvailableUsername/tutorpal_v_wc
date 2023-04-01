61dd2e09bcb12dca6c6dc5431fad056fe706bb32
UnlimitedAvailableUsername <markraileygeneroso@gmail.com>
2023-04-01 15:27:07 +0800

    asdas

--- frontend/src/components/elements/TutorOnCard.js
+++ frontend/src/components/elements/TutorOnCard.js
@@ -7,55 +7,55 @@ import ReadMore from "./Readmore";
 function Tutor({ user }) {
   return (
     <Row>
-      <Card
-        style={{ backgroundColor: "#404040", width: 5000 }}
-        className=" sm- my-10 p-3 rounded"
-      >
+      <Card style={{ backgroundColor: "#404040" }} className="my-10 p-3 rounded" >
         <Row>
-          <Link to={`tutor/${user.id}`}>
-            <Card.Img style={{ height: 303 }} src={`${user.profile_picture}`} />
-          </Link>
-
-          <Card.Body style={{ width: 950 }}>
+          <Col>
             <Link to={`tutor/${user.id}`}>
-              <Card.Title>
-                <strong>
-                  {user.first_name} {user.last_name}
-                </strong>
-              </Card.Title>
+              <Card.Img src={`${user.profile_picture}`} />
             </Link>
-            <Card.Text as="div">
-              <div></div>
-              <div>
-                {" "}
-                Main Subject: {user.subject} <br></br>{" "}
-              </div>
-            </Card.Text>
+          </Col>
+          <Col>
+            <Card.Body>
+              <Link to={`tutor/${user.id}`}>
+                <Card.Title>
+                  <strong>
+                    {user.first_name} {user.last_name}
+                  </strong>
+                </Card.Title>
+              </Link>
+              <Card.Text as="div">
+                <div></div>
+                <div>
+                  {" "}
+                  Main Subject: {user.subject} <br></br>{" "}
+                </div>
+              </Card.Text>
 
-            <Card.Text style={{ color: "#D3D3D3" }}>
-              <div>
-                {user.bio.slice(0, 200)}
-                {user.bio.length > 200 ? <span>...</span> : null}
-              </div>
-              {user.bio.length > 200 ? (
-                <Link to={`tutor/${user.id}`} style={{ color: "#f8e825" }}>
-                  Read More
-                </Link>
-              ) : null}
-            </Card.Text>
+              <Card.Text style={{ color: "#D3D3D3" }}>
+                <div>
+                  {user.bio.slice(0, 200)}
+                  {user.bio.length > 200 ? <span>...</span> : null}
+                </div>
+                {user.bio.length > 200 ? (
+                  <Link to={`tutor/${user.id}`} style={{ color: "#f8e825" }}>
+                    Read More
+                  </Link>
+                ) : null}
+              </Card.Text>
 
-            <Card.Text as="div">
-              <div className="my-3">
-                <Rating
-                  value={user.rating}
-                  text={`${user.numReviews} reviews`}
-                  color={"#f8e825"}
-                />
-              </div>
-            </Card.Text>
+              <Card.Text as="div">
+                <div className="my-3">
+                  <Rating
+                    value={user.rating}
+                    text={`${user.numReviews} reviews`}
+                    color={"#f8e825"}
+                  />
+                </div>
+              </Card.Text>
 
-            <Card.Text as="h3">${user.price_rate_hour} /hr</Card.Text>
-          </Card.Body>
+              <Card.Text as="h3">₱ {user.price_rate_hour}/hr</Card.Text>
+            </Card.Body>
+          </Col>
         </Row>
       </Card>
     </Row>

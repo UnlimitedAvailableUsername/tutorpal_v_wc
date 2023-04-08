4b6747b0a4fee30d33e6495f4f305bc4cbb0ece1
scruBB <mjmendez0529201@gmail.com>
2023-04-08 16:32:14 +0800

    Merge branch 'master-clone' of https://github.com/UnlimitedAvailableUsername/tutorpal_v_wc

--- frontend/src/features/redux/reducers/authUserReducer.js
+++ frontend/src/features/redux/reducers/authUserReducer.js
@@ -1,99 +1,55 @@
 import * as actionType from '../constants/authConstants';
 // Pls wag nyo na galawin ito if di niyo gets
 
-const userLoginInitialState = {
+const userInitialState = {
     loading: false,
     error: null,
     userInfo: null,
+    success: null
 };
 
-export const userLoginReducer = (state = userLoginInitialState, action) => {
-	switch (action.type) {
-
-		case actionType.USER_LOGIN_REQUEST:
-			return {
-                ...state,
-				loading: true,
-			};
-
-		case actionType.USER_LOGIN_SUCCESS:
-			return {
-                ...state,
-				loading: false,
-				userInfo: action.payload,
-			};
-
-		case actionType.USER_LOGIN_FAIL:
-			return {
-                ...state,
-				loading: false,
-				error: action.payload,
-			};
-
-		case actionType.USER_LOGOUT:
-			return {};
-
-		default:
-			return state;
-
-	}
-};
-
-
-/* USER REGISTRATION REDUCER THAT WILL BE USED IN REGISTER SCREEN */
-
-const userRegisterInitialState = {
-    loading: false,
-    success: null,
-    error: null,
-};
-
-export const userRegisterReducer = (state = userRegisterInitialState, action) => {
+export const userReducer = (state = userInitialState, action) => {
     switch (action.type) {
-
+        case actionType.USER_LOGIN_REQUEST:
         case actionType.USER_REGISTER_REQUEST:
+        case actionType.USER_DETAILS_REQUEST:
             return {
                 ...state,
-                loading: true,
+                loading: true
+            };
+
+        case actionType.USER_LOGIN_SUCCESS:
+        case actionType.USER_DETAILS_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                userInfo: action.payload
             };
 
         case actionType.USER_REGISTER_SUCCESS:
             return {
                 ...state,
-                success: true,
-                error: false,
+                loading: false,
+                success: true
             };
 
+        case actionType.USER_LOGIN_FAIL:
         case actionType.USER_REGISTER_FAIL:
+        case actionType.USER_DETAILS_FAIL:
             return {
                 ...state,
-                success: false,
-                error: action.payload,
-            }
+                loading: false,
+                error: action.payload
+            };
 
-        default:
-            return state;
-
-    };
-}
-
-
-/* REDUCER USED IN UPDATING USER DETAILS IN ProfileScreen COMPONENT */
-
-const profileInitialState = {
-    loading: false,
-    success: null,
-    userInfo: null,
-    error: null,
-};
-
-export const userUpdateProfileReducer = (state = profileInitialState, action) => {
-    switch (action.type) {
+        case actionType.USER_LOGOUT:
+            return {};
 
         case actionType.USER_UPDATE_PROFILE_REQUEST:
             return {
                 ...state,
                 loading: true,
+                success: null
             };
 
         case actionType.USER_UPDATE_PROFILE_SUCCESS:
@@ -101,7 +57,7 @@ export const userUpdateProfileReducer = (state = profileInitialState, action) =>
                 ...state,
                 loading: false,
                 success: true,
-                userInfo: action.payload,
+                userInfo: action.payload
             };
 
         case actionType.USER_UPDATE_PROFILE_FAIL:
@@ -109,52 +65,16 @@ export const userUpdateProfileReducer = (state = profileInitialState, action) =>
                 ...state,
                 loading: false,
                 success: false,
-                error: action.payload,
+                error: action.payload
             };
 
         case actionType.USER_UPDATE_PROFILE_RESET:
             return {
                 ...state,
-            }; /* RESET STATE */
-
-        default:
-            return state;
-        
-    };
-};
-
-/* REDUCER USED IN GETTING USER DETAILS IN ProfileScreen COMPONENT */
-
-export const userDetailsReducer = (state = profileInitialState, action) => {
-    switch (action.type) {
-
-        case actionType.USER_DETAILS_REQUEST:
-            return {
-                ...state,
-                loading: true,
-            };
-
-        case actionType.USER_DETAILS_SUCCESS:
-            return {
-                ...state,
-                loading: false,
-                user: action.payload,
-            };
-
-        case actionType.USER_DETAILS_FAIL:
-            return {
-                ...state,
-                loading: false,
-                error: action.payload,
-            };
-
-        case actionType.USER_DETAILS_RESET:
-            return {
-                ...state,
+                success: null
             };
 
         default:
             return state;
-
-    };
+    }
 };

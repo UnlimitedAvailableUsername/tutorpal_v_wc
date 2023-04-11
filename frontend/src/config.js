f2a197fbd17cee06feb3f4f9f845ab2ae156f6dc
UnlimitedAvailableUsername <81744148+UnlimitedAvailableUsername@users.noreply.github.com>
2023-04-11 09:18:11 +0800

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'http://127.0.0.1:8000'
-// export const BASE_URL = 'https://tutorpal.pythonanywhere.com'
+// export const BASE_URL = 'http://127.0.0.1:8000'
+export const BASE_URL = 'https://tutorpal.pythonanywhere.com'
 
 /* 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 */

707b7c96939f16b815f435433bd0422659030048
Marc000001 <mjbeltran072202@gmail.com>
2023-04-15 21:18:26 +0800

    Sorting of Contact us/ Concern

--- frontend/src/components/screens/ListContactsScreen/ListContactdone.js
+++ frontend/src/components/screens/ListContactsScreen/ListContactdone.js
@@ -3,13 +3,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { listContacts } from "../../../features/redux/actions/contactActions";
 import { Button, Col, Container, Form, Row, Table } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
+import Dropdown from "react-bootstrap/Dropdown";
 import HeaderTutor from "../../elements/HeaderTutor";
-
-function ListContactdone() {
+function ListContact() {
   const [search, setSearch] = useState("");
   const [expandedComments, setExpandedComments] = useState([]);
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [sortOrder, setSortOrder] = useState("asc");
 
   const contactList = useSelector((state) => state.contactList);
   const { contacts } = contactList || {};
@@ -34,6 +35,21 @@ function ListContactdone() {
     }
   };
 
+  const sortContactByDate = (contacts) => {
+    if (sortOrder === "asc") {
+      return contacts.sort(
+        (a, b) => new Date(a.created_date) - new Date(b.created_date)
+      );
+    } else if (sortOrder === "desc") {
+      return contacts.sort(
+        (a, b) => new Date(b.created_date) - new Date(a.created_date)
+      );
+    }
+  };
+
+  const handleSortOrderChange = (eventKey) => {
+    setSortOrder(eventKey);
+  };
   return (
     <Container>
       <div className="tutor-text text-center">
@@ -54,12 +70,45 @@ function ListContactdone() {
         </Row>
       </Form>
 
-      <p>Total concerns: {contacts && contacts.filter(contact => contact.done).length}</p>
+      <p>
+        Total concerns:{" "}
+        {contacts && contacts.filter((contact) => contact.done).length}
+      </p>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Dropdown onSelect={handleSortOrderChange}>
+          <Dropdown.Toggle
+            id="dropdown-basic"
+            style={{ backgroundColor: "#037d50 ", borderColor: "#037d50" }}
+          >
+            Sort by date ({sortOrder === "asc" ? "low to high" : "high to low"})
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            <Dropdown.Item eventKey="asc">Oldest</Dropdown.Item>
+            <Dropdown.Item eventKey="desc">Newest</Dropdown.Item>
+          </Dropdown.Menu>
+        </Dropdown>
+
+        <Link to={`/concern-list`}>
+          <Button
+            variant="warning"
+            style={{ fontSize: "0.8rem", padding: "0.2rem 0.5rem" }}
+            className="btn-outline-dark py-2 px-3 my-5"
+          >
+            unDone Concerns
+          </Button>
+        </Link>
+      </div>
 
       <Table striped responsive className="table-m-2">
         <thead>
           <tr>
-            <th>Name</th>
+            <th>Name and Date</th>
             <th>Concern</th>
             <th>Details</th>
             <th>Action</th>
@@ -67,7 +116,7 @@ function ListContactdone() {
         </thead>
         <tbody>
           {contacts &&
-            contacts
+            sortContactByDate(contacts)
               .filter((contact) => {
                 return search.toLowerCase() === ""
                   ? contact
@@ -79,7 +128,10 @@ function ListContactdone() {
               .filter((contact) => contact.done)
               .map((contact) => (
                 <tr key={contact.id}>
-                  <td>{contact.name}</td>
+                  <td>
+                    {contact.name} <br></br>
+                    {contact.created_date}
+                  </td>
                   <td>{contact.concern}</td>
                   <td className="comment-cell">
                     <div className="comment">
@@ -132,4 +184,4 @@ function ListContactdone() {
   );
 }
 
-export default ListContactdone;
+export default ListContact;

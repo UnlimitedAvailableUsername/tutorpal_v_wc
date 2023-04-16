2c3267095e580bf9908f38aa71c6d89b8138b5a7
UnlimitedAvailableUsername <markraileygeneroso@gmail.com>
2023-04-16 16:04:41 +0800

    the limit does not exist

--- frontend/src/components/screens/DoesNotExistScreen/DoesNotExistScreen.js
+++ frontend/src/components/screens/DoesNotExistScreen/DoesNotExistScreen.js
@@ -1,13 +1,21 @@
 import { Container } from "react-bootstrap";
 import logo from "../../../assets/components/screens/DoesNotExist/404.png"
+import background from "../../../assets/components/screens/DoesNotExist/bg.png"
 
 function DoesNotExistScreen() {
   return (
-    <div>
-      <Container className="d-flex flex-column justify-content-center align-items-center" >
+    <div style={{ 
+      backgroundImage: `url(${background})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center center',
+      backgroundSize: 'cover',
+      minHeight: '100vh',
+      padding: '8em 0em 8em 0em',
+    }}>
+      <Container className="d-flex flex-column justify-content-center align-items-center">
         <h1>404 - Not Found!</h1>
         <p>Sorry, the page you are looking for does not exist.</p>
-		<img alt="404" style={{ height: "18em", width: "18em" }} src={logo} />
+        <img alt="404" style={{ height: "18em", width: "18em" }} src={logo} />
       </Container>
     </div>
   );

de23a096362f5a7d1df288a546b43501146b6f42
Marc000001 <mjbeltran072202@gmail.com>
2023-04-19 09:34:11 +0800

    Subject (add, List, Delete)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -62,7 +62,7 @@ function App() {
                             <Route path="/schedule-details/:scheduleId" element={<EditSchedule />} exact />
                             <Route path="/tutors-admit" element={<AdminTutor />} exact />
                             <Route path="/tutors-admit/details/:tutorId" element={<AdminTutorDetail />} exact />
-                            <Route path="/add-subject" element={<AddSubject />} exact />
+                            <Route path="/subject-admin" element={<AddSubject />} exact />
                         </>
                     ) : (
                         null

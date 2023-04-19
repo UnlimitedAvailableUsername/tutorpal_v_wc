60f60dd12a69b097d468a030f6fc96f131c3603f
UnlimitedAvailableUsername <markraileygeneroso@gmail.com>
2023-04-19 23:20:52 +0800

    order up

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,4 +1,7 @@
 export const BASE_URL = 'http://127.0.0.1:8000'
 // export const BASE_URL = 'https://tutorpal.pythonanywhere.com'
 
+// PayPal Client ID Developer:
+export const PAYPAL_CLIENT_ID = 'AVMm3SdGZL1XlpAZt8NDyKKlW2OuukTkALo8JwcisVVuSnlPNLOHb0COfgH6DSeMePX-lvkjdKc5d5FF'
+
 /* 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 */

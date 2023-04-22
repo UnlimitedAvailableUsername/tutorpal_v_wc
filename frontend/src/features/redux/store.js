4f57954c9dba3ad56f142aebfa39ac65b0b96111
UnlimitedAvailableUsername <markraileygeneroso@gmail.com>
2023-04-22 21:57:12 +0800

    merged

--- frontend/src/features/redux/store.js
+++ frontend/src/features/redux/store.js
@@ -3,13 +3,8 @@ import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 import { userLoginReducer, userUpdateProfileReducer, userRegisterReducer, initialLoginState, } from "./reducers/authUserReducer";
-<<<<<<< HEAD
-import { tutorListReducer, tutorDetailsReducer, admintutorListReducer, MyStudentListReducer, studentOrderDetailsReducer } from "./reducers/tutorReducer";
-import { adminUserListReducer, adminReviewListReducer, userDetailsReducer} from "./reducers/adminReducer";
-=======
 import { tutorListReducer, tutorDetailsReducer, admintutorListReducer } from "./reducers/tutorReducer";
-import { adminUserListReducer, adminReviewListReducer} from "./reducers/adminReducer";
->>>>>>> 9101024ee8e4cc8d3276306427e910ede725e21f
+import { adminUserListReducer, adminReviewListReducer, userDetailsReducer} from "./reducers/adminReducer";
 import { scheduleListReducer, scheduleDetailsReducer, } from "./reducers/scheduleReducer";
 import { addContactReducer } from "./reducers/contactReducer";
 import { contactDetailsReducer, contactListReducer, } from "./reducers/contactReducer";

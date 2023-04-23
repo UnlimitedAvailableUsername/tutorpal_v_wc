f1dc2782ffbafb1915b61c66f20a2b3929828471
Marc000001 <mjbeltran072202@gmail.com>
2023-04-23 23:45:03 +0800

    How To guidelines for Tutor Video Sending

---
+++ frontend/src/components/screens/ProfileScreen/TutorHowTo.js
@@ -0,0 +1,54 @@
+import React from 'react'
+
+function TutorHowTo() {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1 style={{ color: 'yellow' }}>Video Sending Guidelines:</h1>
+
+      <div style={{ maxWidth: 800, margin: '0 auto' }}>
+        <p>
+          After your session with your student, you are required to send a recording of that meeting as proof of teaching and will also serve as your attendance. Only with that, you will receive your payment for your services. This is to ensure that you attended the meeting.
+        </p>
+
+        <p>
+          Note: You are not required to turn on your camera the whole time as long as we see that it is you and that the student is really the one your teaching, so we advise that you open your camera at the beginning of the session and at the end.
+          Also keep the recording as raw as possible
+        </p>
+
+        <p>
+          Here are the guidelines for sending the recorded meeting:
+        </p>
+
+        <p>
+          Email the recording to tutorpal@gmail.com
+        </p>
+
+        <p>
+          Subject: Name, Student, Subject You taught
+        </p>
+
+        <p>
+          Insert Video Recording: G-Drive Link of the zoom meet
+        </p>
+
+        <p>
+          Insert where you want to receive your payment: Gcash, Paypal, or Bank
+        </p>
+
+        <p>
+          Note: Make sure you are sending the right account name and ID for the payment. If it is wrong, we are not responsible for that mistake.
+        </p>
+
+        <p>
+          After sending that, we will email you back if it gets verified, and your payment will be sent upon verification.
+        </p>
+
+        <p>
+          If you have any more questions, you can contact us.
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default TutorHowTo
